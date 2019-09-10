8708499944fcb9c6c1eedd07a8bdff0777f45b9d
bsjo <qjatjr1108@gmail.com>
2019-09-10 19:02:11 +0900

    init

---
+++ app/src/main/java/k/bs/designpatternsp/pattern/EagerSingleton.java
@@ -0,0 +1,11 @@
+package k.bs.designpatternsp.pattern;
+
+public class EagerSingleton {
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    private EagerSingleton() {}
+
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
