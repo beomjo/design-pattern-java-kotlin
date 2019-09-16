05de9ba4bd44d92aac466d4438d9817f073b56ec
bsjo <qjatjr1108@gmail.com>
2019-09-16 17:03:56 +0900

    abstract factory pattern java ex 1

---
+++ app/src/main/java/k/bs/designpatternsp/pattern/abstractfactory/ex1/factory/machines/a/MachineA.java
@@ -0,0 +1,5 @@
+package k.bs.designpatternsp.pattern.abstractfactory.ex1.factory.machines.a;
+
+public interface MachineA {
+    public void process();
+}
---
+++ app/src/main/java/k/bs/designpatternsp/pattern/abstractfactory/ex1/factory/machines/b/MachineB.java
@@ -0,0 +1,5 @@
+package k.bs.designpatternsp.pattern.abstractfactory.ex1.factory.machines.b;
+
+public interface MachineB {
+    public void process();
+}
