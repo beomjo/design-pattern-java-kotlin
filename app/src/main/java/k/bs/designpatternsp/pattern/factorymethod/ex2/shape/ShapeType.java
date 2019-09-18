29c8cc1c07ee7095c095a48b87cdb4d94380773b
bsjo <qjatjr1108@gmail.com>
2019-09-18 13:15:38 +0900

    factory method pattern java ex2

---
+++ app/src/main/java/k/bs/designpatternsp/pattern/factorymethod/ex2/shape/ShapeType.java
@@ -0,0 +1,5 @@
+package k.bs.designpatternsp.pattern.factorymethod.ex2.shape;
+
+public enum ShapeType {
+    CIRCLE,RECTANGLE,SQUARE
+}
---
+++ app/src/main/java/k/bs/designpatternsp/pattern/factorymethod/ex2/shape/Square.java
@@ -0,0 +1,8 @@
+package k.bs.designpatternsp.pattern.factorymethod.ex2.shape;
+
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Square - draw");
+    }
+}
