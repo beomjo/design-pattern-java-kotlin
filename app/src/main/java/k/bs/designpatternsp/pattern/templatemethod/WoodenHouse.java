4fd55ccd55487b392824396e54ccfbb8587771a8
bsjo <qjatjr1108@gmail.com>
2019-09-19 10:55:59 +0900

    template method pattern

---
+++ app/src/main/java/k/bs/designpatternsp/pattern/templatemethod/WoodenHouse.java
@@ -0,0 +1,13 @@
+package k.bs.designpatternsp.pattern.templatemethod;
+
+public class WoodenHouse extends HouseTemplate {
+    @Override
+    public void buildWalls() {
+        System.out.println("Building Wooden Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Building Pillars with Wood coating");
+    }
+}
